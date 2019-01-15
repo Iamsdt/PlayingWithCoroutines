216675064847904715fc9dce2f1ddfeb69846c74
Shudipto <Shudiptotrafder@gmail.com>
2019-01-15 23:28:05 +0600

    cancellation
    Took 19 minutes

---
+++ src/CancellationAndTimeouts.kt
@@ -0,0 +1,63 @@
+import kotlinx.coroutines.*
+
+fun main() {
+    cancel3()
+}
+
+// Making computation code cancellable
+fun cancel3() = runBlocking {
+    val startTime = System.currentTimeMillis()
+
+    val job = launch(Dispatchers.Default){
+        var nextPrintTime = startTime
+        var i = 0
+        while (isActive) { // computation loop, just wastes CPU
+            // print a message twice a second
+            if (System.currentTimeMillis() >= nextPrintTime) {
+                println("I'm sleeping ${i++} ...")
+                nextPrintTime += 500L
+            }
+        }
+    }
+
+    delay(1300L) // delay a bit
+    println("main: I'm tired of waiting!")
+    job.cancelAndJoin() // cancels the job and waits for its completion
+    println("main: Now I can quit.")
+}
+
+//Cancellation is cooperative
+fun cancel2() = runBlocking {
+    val startTime = System.currentTimeMillis()
+
+    val job = launch(Dispatchers.Default){
+        var nextPrintTime = startTime
+        var i = 0
+        while (i < 5) { // computation loop, just wastes CPU
+            // print a message twice a second
+            if (System.currentTimeMillis() >= nextPrintTime) {
+                println("I'm sleeping ${i++} ...")
+                nextPrintTime += 500L
+            }
+        }
+    }
+
+    delay(1300L) // delay a bit
+    println("main: I'm tired of waiting!")
+    job.cancelAndJoin() // cancels the job and waits for its completion
+    println("main: Now I can quit.")
+}
+
+fun cancel1() = runBlocking {
+    val job = launch {
+        repeat(1000) { i ->
+            println("I'm sleeping $i ...")
+            delay(500L)
+        }
+    }
+    delay(1300L) // delay a bit
+    println("main: I'm tired of waiting!")
+    job.cancel() // cancels the job
+    job.join() // waits for job's completion
+    println("main: Now I can quit.")
+}
